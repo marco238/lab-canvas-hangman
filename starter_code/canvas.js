aa50a2458a1660dcbb54eaf8c3b92845652c073e
Marcos Monzon <marco238cuba@gmail.com>
2017-11-22 02:07:13 +0100

    feat: Jasmine done

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -86,3 +86,19 @@ HangmanCanvas.prototype._winner = function() {
   };
   img.src = './images/awesome.png';
 };
+
+var hangman;
+var canvas;
+
+document.getElementById("start-game-button").onclick = function() {
+  hangman = new Hangman();
+  canvas = new HangmanCanvas("Hola");
+
+  hangman._getWord();
+  canvas._drawHangman();
+};
+
+document.onkeydown = function(e) {
+  hangman._checkIfLetter(e);
+  canvas._writeCorrectLetter(e);
+};
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,37 @@
 var hangman;
 
 function Hangman() {
-
+  this.words = ['hola', 'marco', 'bye'];
+  this.secretWord = '';
+  this.guessedLetter = '';
+  this.errorsLeft = 10;
+  this.letters = [];
 }
-
-Hangman.prototype._getWord = function () {
-
+Hangman.prototype._getWord = function() {
+  return this.words[Math.floor(Math.random() * this.words.length)];
 };
-
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+  return keyCode >= 65 && keyCode <= 90 ? true : false;
 };
-
 Hangman.prototype._checkClickedLetters = function(key) {
-
+  return !this.letters.includes(key);
 };
-
-Hangman.prototype._addCorrectLetter = function(i){
-
+Hangman.prototype._addCorrectLetter = function(i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
 };
-
-Hangman.prototype._addWrongLetter = function (letter){
-
+Hangman.prototype._addWrongLetter = function(letter) {
+  if (this.secretWord.indexOf(letter) === -1) {
+    this.errorsLeft--;
+  }
 };
-
 Hangman.prototype._checkGameOver = function() {
-
+  return this.errorsLeft === 0 ? true : false;
 };
-
 Hangman.prototype._checkWinner = function() {
-
+  return this.secretWord.length === this.guessedLetter.length ? true : false;
 };
-
 document.getElementById("start-game-button").onclick = function(){
   hangman = new Hangman();
 };
-
-
 document.onkeydown = function(e) {
-
 };
